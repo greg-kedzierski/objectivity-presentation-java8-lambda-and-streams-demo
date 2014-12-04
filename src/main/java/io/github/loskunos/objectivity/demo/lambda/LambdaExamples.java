c1c1ec3ce36aa3c7fe438d256adde1394fd169db
Kedzierski, Grzegorz <grzegorz.kedzierski_external@immobilienscout24.de>
2014-12-04 12:10:06 +0100

    restore correct

--- src/main/java/io/github/loskunos/objectivity/demo/lambda/LambdaExamples.java
+++ src/main/java/io/github/loskunos/objectivity/demo/lambda/LambdaExamples.java
@@ -7,31 +7,31 @@ public class LambdaExamples {
 
 
   public static void main(String[] args) {
-//    Fun fun = (int x, int y) -> x + y;
-//    FunVoid funVoid = (int x, int y) -> {
-//      int i = x + y;
-//    };
-//
-//    Fun fun2 = (x, y) -> x + y;
+    Fun fun = (int x, int y) -> x + y;
+    FunVoid funVoid = (int x, int y) -> {
+      int i = x + y;
+    };
+
+    Fun fun2 = (x, y) -> x + y;
 
     BinaryOperator<Integer> add = (x, y) -> x + y;
 
-//    // effective final
-//    String text = "Hello";
-//    Supplier<String> sup = () -> text;
-//
-//    System.out.println(sup.get());
+    // effective final
+    String text = "Hello";
+    Supplier<String> sup = () -> text;
+
+    System.out.println(sup.get());
   }
 
 
 
-//  interface Fun {
-//    int sum(int a, int b);
-//  }
-//
-//  @FunctionalInterface
-//  interface FunVoid {
-//    void sum(int a, int b);
-//  }
+  interface Fun {
+    int sum(int a, int b);
+  }
+
+  @FunctionalInterface
+  interface FunVoid {
+    void sum(int a, int b);
+  }
 
 }
